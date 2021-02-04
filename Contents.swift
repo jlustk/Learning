b6eb3a5b91da2dfc9b774c2768e0b01c6fbc19ec
Igor <igorlavr@rambler.ru>
2021-02-04 12:47:22 +0200

    5l_LavrynenkoIgor

--- Contents.swift
+++ Contents.swift
@@ -6,85 +6,121 @@ enum engineProgram {
     case start
 }
 
-enum doSome {
-    case openWindows
-    case closeWindows
-    case startEngine
-    case stopEngine
-    case addCargo
-    case updateSpeed
+protocol CarOperation {
+    var cargoSpace: Double {get}
+    var fullCargoSpace: Double {get set}
+    var engine: engineProgram {get set}
+    var windowsIsOpen: Bool {get set}
+    mutating func openWindows(_ operation : Bool) -> Bool
+    func startEngine(_ operation : engineProgram) -> engineProgram
 }
 
-enum ans {
-    case open(msg : String)
-    case close(msg : String)
-    case startE(msg : String)
-    case stopE(msg : String)
-    case tryAddCargo(msg: String)
-    case newSpeed(msg: String)
-    case notCoise(msg : String)
+extension CarOperation {
+    func getSpace(_ add: Double) -> Bool {
+        if fullCargoSpace + add <= cargoSpace {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    mutating func openWindows(_ operation : Bool) -> Bool {
+        print("extention super class")
+        if operation {
+            self.windowsIsOpen = true
+        } else {
+            self.windowsIsOpen = false
+        }
+        print(windowsIsOpen)
+        return windowsIsOpen
+        }
 }
 
-class Car {
+class Car: CarOperation {
     let model: String
     let year: Int
     let cargoSpace: Double
     var engine: engineProgram
     var windowsIsOpen: Bool
-    
+    var fullCargoSpace: Double
+
     init(model: String, year: Int, cargoSpace: Double) {
         self.model = model
         self.year = year
         self.cargoSpace = cargoSpace
         self.engine = .stop
         self.windowsIsOpen = false
+        self.fullCargoSpace = 0.0
     }
-    
-    func vehicleOperations(_ operation : doSome, _ : Double = 0.0) -> ans {
-        return(.notCoise(msg: "Vehicle not choise"))
+
+    func startEngine(_ operation : engineProgram) -> engineProgram {
+        self.engine = operation
+        return self.engine
     }
 }
 
+var bmw1 = Car(model: "BMW", year: 2021, cargoSpace: 100)
+print(bmw1.startEngine(.start))
+print(bmw1.openWindows(true))
+print(bmw1.getSpace(120))
+
 class TrunkCar : Car {
-    var fullCargoSpace: Double = 0.0
-    
+    var trailer: Bool
+    var trailerSpace: Double
+    var fullTrailerSpace: Double
+
     init(model: String, year: Int, cargoSpace: Double, fullCargoSpace: Double) {
-        self.fullCargoSpace = fullCargoSpace
+        self.trailer = false
+        self.trailerSpace = 0.0
+        self.fullTrailerSpace = 0.0
         super.init(model: model, year: year, cargoSpace: cargoSpace)
+        self.fullCargoSpace = fullCargoSpace
+    }
+    
+    func checkTrailer (_ add: Double) -> String {
+        if fullTrailerSpace + add >= trailerSpace {
+            return "We ran out of free space! We cannot load even using a trailer"
+        } else {
+            fullTrailerSpace += add
+            return "Cargo loads, free space \(trailerSpace - fullTrailerSpace)"
+        }
+    }
+    
+    func fullTrailer (_ add: Double) -> String {
+        if !trailer {
+            trailer = true
+            trailerSpace = cargoSpace
+        }
+        let answer: String = checkTrailer(add)
+        return answer
     }
 
-    override func vehicleOperations(_ operation : doSome, _ fullCargo : Double = 0.0) -> ans {
-        switch operation {
-        case .openWindows:
-            self.windowsIsOpen = true
-            return .open(msg: "Windows is open")
-        case .closeWindows:
-            self.windowsIsOpen = false
-            return .open(msg: "Windows is close")
-        case .startEngine:
-            self.engine = .start
-            return .startE(msg: "Engine start")
-        case .stopEngine:
-            self.engine = .stop
-            return .stopE(msg: "Engine stop")
-        case .addCargo:
-            var answer: String
-            if self.fullCargoSpace + fullCargo <= cargoSpace {
-                self.fullCargoSpace += fullCargo
-                answer = "Cargo add to vehicle, empty space \(self.cargoSpace - self.fullCargoSpace)"
-            } else {
-                answer = "Cargo don't add to vehicle, empty space \(self.cargoSpace - self.fullCargoSpace)"
-            }
-            return .tryAddCargo(msg: answer)
-        case .updateSpeed:
-            return .newSpeed(msg: "Not fast")
+    func load (_ add: Double) {
+        if self.getSpace(add) {
+            print(self.fullCargoSpace + add)
+            self.fullCargoSpace += add
+        } else {
+            print(self.fullTrailer(add))
         }
     }
 }
 
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "Trailer connected - \(trailer)"
+    }
+}
+
 class SportCar : Car {
-    var speed: UInt = 0
-    
+    var speed: UInt = 0 {
+        willSet {
+            print(newValue)
+        }
+        didSet {
+            print(oldValue)
+        }
+    }
+
     init(model: String, year: Int, cargoSpace: Double, speed: UInt) {
         self.speed = speed
         super.init(model: model, year: year, cargoSpace: cargoSpace)
@@ -92,32 +128,29 @@ class SportCar : Car {
             self.engine = .start
         }
     }
-
-    override func vehicleOperations(_ operation : doSome, _ chengeSpeed : Double = 0.0) -> ans {
-        switch operation {
-        case .openWindows:
-            self.windowsIsOpen = true
-            return .open(msg: "Windows is open")
-        case .closeWindows:
-            self.windowsIsOpen = false
-            return .open(msg: "Windows is close")
-        case .startEngine:
-            self.engine = .start
-            return .startE(msg: "Engine start")
-        case .stopEngine:
-            self.engine = .stop
-            return .stopE(msg: "Engine stop")
-        case .addCargo:
-            return .tryAddCargo(msg: "Sport car not for uploads cargo")
-        case .updateSpeed:
-            self.speed = UInt(chengeSpeed)
-            return .newSpeed(msg: "New speed is \(chengeSpeed)")
-            
-        }
+    
+    func updateSpeed (_ new: UInt) {
+        self.speed += new
     }
 }
 
-var bmw = SportCar(model: "bmw", year: 2021, cargoSpace: 10, speed: 100)
-print(bmw.vehicleOperations(.updateSpeed, 140))
+extension SportCar: CustomStringConvertible {
+    var description: String {
+        return "Now our speed is \(speed)"
+    }
+}
+
+var bmw = SportCar(model: "bmw", year: 2021, cargoSpace: 10, speed: 10)
+print(bmw.description)
+bmw.updateSpeed(10)
+print(bmw.description)
+bmw.updateSpeed(20)
+print(bmw)
 var volvo = TrunkCar(model: "Volvo", year: 2021, cargoSpace: 1200, fullCargoSpace: 1000)
-print(volvo.vehicleOperations(.addCargo, 200))
+print(volvo.cargoSpace, volvo.fullCargoSpace)
+volvo.load(200)
+print(volvo)
+volvo.load(1100)
+print(volvo)
+volvo.openWindows(true)
+bmw.openWindows(true)
