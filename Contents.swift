8d1813a4547e17b9256ed384ac94e18277eab147
Igor <igorlavr@rambler.ru>
2021-02-05 12:55:28 +0200

    6l_LavrynenkoIgor

--- Contents.swift
+++ Contents.swift
@@ -1,156 +1,101 @@
 import UIKit
 import Foundation
 
-enum engineProgram {
-    case stop
-    case start
+enum countSetings {
+    case login
+    case logout
 }
 
-protocol CarOperation {
-    var cargoSpace: Double {get}
-    var fullCargoSpace: Double {get set}
-    var engine: engineProgram {get set}
-    var windowsIsOpen: Bool {get set}
-    mutating func openWindows(_ operation : Bool) -> Bool
-    func startEngine(_ operation : engineProgram) -> engineProgram
+protocol Screen {
+    var memory: Int {get set}
 }
 
-extension CarOperation {
-    func getSpace(_ add: Double) -> Bool {
-        if fullCargoSpace + add <= cargoSpace {
-            return true
-        } else {
-            return false
-        }
+class ScreenMenu: Screen {
+    var memory: Int
+    var settings: countSetings
+    var background: String
+    
+    init(settings: countSetings, background: String, memory: Int) {
+        self.settings = settings
+        self.background = background
+        self.memory = memory
+    }
+}
+
+class ScreenImage: Screen {
+    var memory: Int
+    var imageName: String
+    var background: String
+    
+    init(imageName: String, memory: Int) {
+        self.imageName = imageName
+        self.background = "blue"
+        self.memory = memory
+    }
+}
+
+struct ScreenSet<T: Screen> {
+    var openScreen: [T] = []
+    
+    mutating func push(_ screen: T) {
+        openScreen.append(screen)
     }
     
-    mutating func openWindows(_ operation : Bool) -> Bool {
-        print("extention super class")
-        if operation {
-            self.windowsIsOpen = true
-        } else {
-            self.windowsIsOpen = false
+    mutating func pop() -> T? {
+        guard openScreen.count > 0 else {
+            return nil
         }
-        print(windowsIsOpen)
-        return windowsIsOpen
-        }
-}
-
-class Car: CarOperation {
-    let model: String
-    let year: Int
-    let cargoSpace: Double
-    var engine: engineProgram
-    var windowsIsOpen: Bool
-    var fullCargoSpace: Double
-
-    init(model: String, year: Int, cargoSpace: Double) {
-        self.model = model
-        self.year = year
-        self.cargoSpace = cargoSpace
-        self.engine = .stop
-        self.windowsIsOpen = false
-        self.fullCargoSpace = 0.0
-    }
-
-    func startEngine(_ operation : engineProgram) -> engineProgram {
-        self.engine = operation
-        return self.engine
+        return openScreen.removeLast()
     }
 }
 
-var bmw1 = Car(model: "BMW", year: 2021, cargoSpace: 100)
-print(bmw1.startEngine(.start))
-print(bmw1.openWindows(true))
-print(bmw1.getSpace(120))
-
-class TrunkCar : Car {
-    var trailer: Bool
-    var trailerSpace: Double
-    var fullTrailerSpace: Double
-
-    init(model: String, year: Int, cargoSpace: Double, fullCargoSpace: Double) {
-        self.trailer = false
-        self.trailerSpace = 0.0
-        self.fullTrailerSpace = 0.0
-        super.init(model: model, year: year, cargoSpace: cargoSpace)
-        self.fullCargoSpace = fullCargoSpace
-    }
-    
-    func checkTrailer (_ add: Double) -> String {
-        if fullTrailerSpace + add >= trailerSpace {
-            return "We ran out of free space! We cannot load even using a trailer"
-        } else {
-            fullTrailerSpace += add
-            return "Cargo loads, free space \(trailerSpace - fullTrailerSpace)"
+extension ScreenSet {
+    subscript(indexces: Int...) -> Int {
+        var total = 0
+        for i in indexces where i >= 0 && i < self.openScreen.count {
+            total += self.openScreen[i].memory
         }
-    }
-    
-    func fullTrailer (_ add: Double) -> String {
-        if !trailer {
-            trailer = true
-            trailerSpace = cargoSpace
-        }
-        let answer: String = checkTrailer(add)
-        return answer
-    }
-
-    func load (_ add: Double) {
-        if self.getSpace(add) {
-            print(self.fullCargoSpace + add)
-            self.fullCargoSpace += add
-        } else {
-            print(self.fullTrailer(add))
-        }
+        return total
     }
 }
 
-extension TrunkCar: CustomStringConvertible {
-    var description: String {
-        return "Trailer connected - \(trailer)"
-    }
+let useMinMemory: (Int, Int) -> Bool = {
+    (i: Int, j: Int) -> Bool in return i < j
 }
 
-class SportCar : Car {
-    var speed: UInt = 0 {
-        willSet {
-            print(newValue)
-        }
-        didSet {
-            print(oldValue)
-        }
-    }
-
-    init(model: String, year: Int, cargoSpace: Double, speed: UInt) {
-        self.speed = speed
-        super.init(model: model, year: year, cargoSpace: cargoSpace)
-        if self.speed > 0 {
-            self.engine = .start
+func findMin(obj: ScreenSet<ScreenImage>, predicate: (Int, Int) -> Bool) -> Int {
+    var minUse: Int = 0
+    let len = obj.openScreen.count - 1
+    for i in (0...len) {
+        if minUse == 0 {
+            minUse = obj[i]
+        } else if predicate(obj[i], minUse) {
+            minUse = obj[i]
         }
     }
-    
-    func updateSpeed (_ new: UInt) {
-        self.speed += new
-    }
+    return minUse
 }
 
-extension SportCar: CustomStringConvertible {
-    var description: String {
-        return "Now our speed is \(speed)"
-    }
-}
+var menu = ScreenSet<ScreenMenu>()
+var image = ScreenSet<ScreenImage>()
 
-var bmw = SportCar(model: "bmw", year: 2021, cargoSpace: 10, speed: 10)
-print(bmw.description)
-bmw.updateSpeed(10)
-print(bmw.description)
-bmw.updateSpeed(20)
-print(bmw)
-var volvo = TrunkCar(model: "Volvo", year: 2021, cargoSpace: 1200, fullCargoSpace: 1000)
-print(volvo.cargoSpace, volvo.fullCargoSpace)
-volvo.load(200)
-print(volvo)
-volvo.load(1100)
-print(volvo)
-volvo.openWindows(true)
-bmw.openWindows(true)
+menu.push(ScreenMenu(settings: .login, background: "green", memory: 220))
+image.push(ScreenImage(imageName: "first", memory: 10))
+image.push(ScreenImage(imageName: "two", memory: 23))
+image.push(ScreenImage(imageName: "three", memory: 15))
+menu.push(ScreenMenu(settings: .logout, background: "red", memory: 40))
+
+image[0]
+image[1]
+image[2]
+image[3]
+image.openScreen.count
+useMinMemory(image[0], image[1])
+findMin(obj: image, predicate: useMinMemory)
+
+image.pop()?.imageName
+image.pop()?.imageName
+image.pop()?.imageName
+menu.pop()?.background
+menu.pop()?.background
+menu.pop()?.background
